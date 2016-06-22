d340303a894730601b79816d259e4c50629c378a
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-06-22 12:22:03 -0300

    melhorado script de criação da base

--- poo-library-dao-activejdbc/create.sql
+++ poo-library-dao-activejdbc/create.sql
@@ -1,13 +1,15 @@
 # ---------------------------------------------------------------------
-# Arquivo de criação das tabelas
+# Arquivo de criação da base de dados
 # ---------------------------------------------------------------------
 
+CREATE DATABASE IF NOT EXISTS `biblioteca`;
+
+USE `biblioteca`;
+
 SET storage_engine=InnoDB;
-ALTER DATABASE CHARACTER SET utf8 COLLATE 'utf8_general_ci'
+ALTER DATABASE CHARACTER SET utf8 COLLATE 'utf8_general_ci';
 
-CREATE DATABASE 'biblioteca';
-
-CREATE USER 'biblioteca'@'localhost' IDENTIFIED BY '123456';
+/* CREATE USER 'biblioteca'@'localhost' IDENTIFIED BY '123456';
 
 GRANT USAGE ON *.* TO 'biblioteca'@'localhost';
 
@@ -15,15 +17,17 @@ GRANT
     SELECT, EXECUTE, SHOW VIEW, ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE,
     CREATE TEMPORARY TABLES, CREATE VIEW, DELETE, DROP, EVENT, INDEX, INSERT,
     REFERENCES, TRIGGER, UPDATE, LOCK TABLES
-ON `biblioteca`.* TO 'biblioteca'@'localhost' WITH GRANT OPTION;
+ON `biblioteca`.* TO 'biblioteca'@'localhost' WITH GRANT OPTION; */
+
+DROP TABLE IF EXISTS `usuario`;
 
 CREATE TABLE `usuario` (
 
-	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`nome` VARCHAR(100) NOT NULL,
-	`cpf` CHAR(11) NOT NULL,
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(100) NOT NULL,
+    `cpf` CHAR(11) NOT NULL,
 
-	PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`)
 
 )
 COMMENT '';
