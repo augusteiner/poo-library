515eca7305ff487465dc153337d944838bbc018e
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-06-22 12:20:36 -0300

    melhorias pequenas

--- poo-library-modelo/src/main/java/poo/library/modelo/Usuario.java
+++ poo-library-modelo/src/main/java/poo/library/modelo/Usuario.java
@@ -40,7 +40,13 @@ public class Usuario implements IUsuario {
 
     public Usuario(int id, String nome, String cpf) {
 
+        this(nome, cpf);
+
         this.id = id;
+    }
+
+    public Usuario(String nome, String cpf) {
+
         this.nome = nome;
         this.cpf = cpf;
     }
