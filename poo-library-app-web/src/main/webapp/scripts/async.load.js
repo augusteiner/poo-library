0168c68ef23608ff14dfdf222dc995f86f69c3f6
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-07-14 14:30:54 -0300

    ajustes na queue de scripts

---
+++ poo-library-app-web/src/main/webapp/scripts/async.load.js
@@ -0,0 +1,67 @@
+function queued(selector, options) {
+
+  var queue = [];
+
+  $(selector).each(function() {
+
+    var $this = $(this);
+
+    queue.push($.Deferred().done(function() {
+
+      $this.load(function() {
+
+        if (queue.length > 0) {
+
+          queue.shift().resolve();
+        }
+      });
+
+      $this.prop('src', $this.data('src'));
+
+    }));
+  });
+
+  queue.shift().resolve();
+
+  return queue[queue.length - 1];
+}
+function bulk(selector) {
+
+  var list = [];
+  var dfd = $.Deferred();
+
+  $(selector).each(function() {
+
+    var $this = $(this);
+
+    list.push(1);
+
+    $this.load(function() {
+
+      list.shift();
+
+      if (list.length == 0) {
+
+        dfd.resolve();
+      }
+    });
+
+    $this.prop('src', $this.data('src'));
+  });
+
+  return dfd;
+}
+function bootstrap() {
+
+  var appName = 'biblioteca';
+
+  queued('script.queued[data-src]').done(function() {
+
+    bulk('script.pass-1[data-src]').done(function() {
+
+      $(document).prop('ng-app', appName);
+
+      angular.bootstrap(document, [appName]);
+    });
+  });
+}
