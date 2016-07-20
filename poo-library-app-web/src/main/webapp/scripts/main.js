e57685d1acfff64c7a56749811aec7298413fb5f
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-07-19 23:56:05 -0300

    organizado layout & melhorias no carregamento

---
+++ poo-library-app-web/src/main/webapp/scripts/main.js
@@ -0,0 +1,48 @@
+
+(function() {
+
+  function queued(e,n,r){var t=[],o=$.Deferred().done(function(){t.shift()});
+  return $(e).each(function(){var e=$(this);t.push($.Deferred().done(function(){
+  e.load(function(){t.length>0&&(o.notify(),t.shift().resolve())}),e.prop("src",
+  e.data("src"))}))}),t.push(o),t.shift().resolve(),o}function bulk(e){var n=
+  $.Deferred(),r=$(e),t=r.length;return r.each(function(){queued(this).done(
+  function(){t--,n.notify(),0==t&&n.resolve()})}),n.promise()}
+
+  if (jQuery)
+    $(bootstrap);
+  else
+    document.getElementById('jquery').addEventListener('load', bootstrap);
+
+  function bootstrap() {
+    var APP_NAME = 'biblioteca';
+
+    var progress = $('progress.main-progress');
+    var scripts = $('script.queued');
+    var total = scripts.length;
+
+    $('.dropdown-menu a:not(.dropdown-toggle)').click(function(){
+      $(this).closest('.dropdown-menu').dropdown('toggle');
+    });
+
+    progress.prop('max', total);
+
+    var p = function() {
+      progress.val(progress.val() + 1);
+    };
+
+    var done = function() {
+      $(document).prop('ng-app', APP_NAME);
+      angular.bootstrap(document, [APP_NAME]);
+    };
+
+    bulk('.bulk-1').progress(p).done(function() {
+      bulk('.bulk-2').progress(p).done(function() {
+        bulk('.bulk-3').progress(p).done(function() {
+          bulk('.bulk-4').progress(p).done(function() {
+            bulk('.bulk-5').progress(p).done(done);
+          });
+        });
+      });
+    });
+  }
+})();
