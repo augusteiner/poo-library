19aa52242fe22f64e4e4fd50136f802a24b29fd6
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-07-22 13:44:16 -0300

    adicionados testes da api
    ações sobre itens do acervo
      TODO testar leak em operações de GET e DELETE de acervo

--- poo-library-app-web/tests.js
+++ poo-library-app-web/tests.js
@@ -10,10 +10,10 @@
   chai.use(chaiHttp);
 
   describe('CRUD de Usuários', function() {
-    var usuarioId = null;
+    var entityId = null;
     var ROOT_PATH = '/usuario';
 
-    it('Deve listar todos os usuários em /usuario GET', function(done) {
+    it('deve listar todos os usuários em ' + ROOT_PATH + ' GET', function(done) {
       chai.request(server)
         .get(ROOT_PATH)
         .end(function(err, res){
@@ -26,7 +26,7 @@
         });
     });
 
-    it('Deve cadastrar usuário em /usuario POST', function(done) {
+    it('deve cadastrar usuário em ' + ROOT_PATH + ' POST', function(done) {
       chai.request(server)
         .post(ROOT_PATH)
         .send({
@@ -40,8 +40,8 @@
 
           res.headers.location.should.be.a('string');
 
-          usuarioId = res.headers.location.replace(server, '').replace('/usuario/', '');
-          usuarioId.should.be.above(0);
+          entityId = res.headers.location.replace(server, '').replace(ROOT_PATH + '/', '');
+          entityId.should.be.above(0);
 
           res.should.have.status(201);
 
@@ -49,12 +49,12 @@
         });
     });
 
-    it('Deve alterar usuário em /usuario PUT', function(done) {
+    it('deve alterar usuário em ' + ROOT_PATH + ' PUT', function(done) {
 
       chai.request(server)
-        .put(ROOT_PATH + '/' + usuarioId)
+        .put(ROOT_PATH + '/' + entityId)
         .send({
-          'id' : usuarioId,
+          'id' : entityId,
           'nome' : 'João Maria das Lurdes II',
           'cpf' : '00569875487',
           'endereco' : 'Av. Salgado Filho, 1001',
@@ -69,9 +69,9 @@
         });
     });
 
-    it('Deve deletar usuário em /usuario DELETE', function(done) {
+    it('deve deletar usuário em ' + ROOT_PATH + ' DELETE', function(done) {
       chai.request(server)
-        .delete(ROOT_PATH + '/' + usuarioId)
+        .delete(ROOT_PATH + '/' + entityId)
         .end(function(err, res){
 
           if (err) throw err;
@@ -82,5 +82,183 @@
         });
     });
   });
-  
+
+  describe('CRUD de Bibliotecas', function() {
+    var entityId = null;
+    var ROOT_PATH = '/biblioteca';
+
+    it('deve listar todas as bibliotecas em ' + ROOT_PATH + ' GET', function(done) {
+      chai.request(server)
+        .get(ROOT_PATH)
+        .end(function(err, res){
+
+          if (err) throw err;
+
+          res.should.have.status(200);
+
+          done();
+        });
+    });
+
+    it('deve cadastrar biblioteca em ' + ROOT_PATH + ' POST', function(done) {
+
+      chai.request(server)
+        .post(ROOT_PATH)
+        .send({
+          'nome' : 'iBook Store - Lagoa Nova',
+          'endereco' : 'Av. Salgado Filho, 1001',
+          'qteDiasValidadeReserva': 3,
+          'qteDiasLocacao': 14,
+          'multaDiaria': '2.55'})
+        .end(function(err, res){
+
+          if (err) throw err;
+
+          res.headers.location.should.be.a('string');
+
+          entityId = res.headers.location.replace(server, '').replace(ROOT_PATH + '/', '');
+          entityId.should.be.above(0);
+
+          res.should.have.status(201);
+
+          done();
+        });
+
+    });
+
+    it('deve alterar biblioteca em ' + ROOT_PATH + ' PUT', function(done) {
+
+      chai.request(server)
+        .put(ROOT_PATH + '/' + entityId)
+        .send({
+          'id' : entityId,
+          'nome' : 'iBook Store II - Lagoa Nova',
+          'endereco' : 'Av. Salgado Filho, 1001',
+          'qteDiasValidadeReserva': 3,
+          'qteDiasLocacao': 14,
+          'multaDiaria': '2.55'})
+        .end(function(err, res){
+
+          if (err) throw err;
+
+          res.should.have.status(200);
+
+          done();
+        });
+    });
+
+    describe(':: Itens de Acervo', function() {
+      var SUB_ROOT_PATH = '/biblioteca/{bibliotecaId}/acervo';
+      var getRootPath = function() {
+
+        // console.log('/biblioteca/' + entityId + '/acervo');
+
+        return '/biblioteca/' + entityId + '/acervo';
+      };
+
+      var subEntityId = null;
+
+//      it('deve listar todo o acervo em ' + SUB_ROOT_PATH + ' GET', function(done) {
+//
+//        chai.request(server)
+//          .get(getRootPath())
+//          .end(function(err, res){
+//
+//            if (err) throw err;
+//
+//            res.should.have.status(200);
+//
+//            done();
+//        });
+//
+//      });
+
+      it('deve cadastrar acervo em ' + SUB_ROOT_PATH + ' POST', function(done) {
+
+        chai.request(server)
+          .post(getRootPath())
+          .send({
+            'bibliotecaId': entityId,
+            'titulo' : 'Use a cabeça! C#',
+            'autor' : 'GONÇALVES, Eleonor Alves',
+            'precoLocacao' : 12.55,
+            'qteDisponivel' : 20,
+            'qteTotal' : 20,
+            'categoria': 'LIVRO'})
+          .end(function(err, res){
+
+            if (err) throw err;
+
+            // console.log(res.req);
+
+            res.headers.location.should.be.a('string');
+
+            subEntityId = res.headers.location.replace(server, '').replace(getRootPath() + '/', '');
+            subEntityId.should.be.above(0);
+
+            console.log(subEntityId);
+
+            res.should.have.status(201);
+
+            done();
+          });
+
+      });
+
+      it('deve alterar acervo em ' + SUB_ROOT_PATH + ' PUT', function(done) {
+
+        chai.request(server)
+          .put(getRootPath() + '/' + subEntityId)
+          .send({
+            'id' : subEntityId,
+            'bibliotecaId': entityId,
+            'titulo' : 'Use a cabeça! C#',
+            'autor' : 'GONÇALVES, Eleonor Alves',
+            'precoLocacao' : 12.55,
+            'qteDisponivel' : 20,
+            'qteTotal' : 20,
+            'categoria': 'LIVRO'})
+          .end(function(err, res){
+
+            if (err) throw err;
+
+            res.should.have.status(200);
+
+            done();
+          });
+
+      });
+
+//    it('deve deletar acervo em ' + SUB_ROOT_PATH + ' DELETE', function(done) {
+//
+//      chai.request(server)
+//        .delete(getRootPath() + '/' + subEntityId)
+//        .end(function(err, res){
+//
+//          if (err) throw err;
+//
+//          res.should.have.status(200);
+//
+//          done();
+//      });
+//
+//    });
+
+      it('deve deletar biblioteca em ' + ROOT_PATH + ' DELETE', function(done) {
+        chai.request(server)
+          .delete(ROOT_PATH + '/' + entityId)
+          .end(function(err, res){
+
+            if (err) throw err;
+
+            res.should.have.status(200);
+
+            done();
+          });
+      });
+
+    });
+
+  });
+
 })();
